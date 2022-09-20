2ed191b7cd9bae3f1c6858dca3b5f783142f0e72
farhan sharif <farhansharif1410@gmail.com>
2022-09-20 19:53:15 +0530

    arry in react map practice

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@
 import './App.css';
 
 function App() {
+
+  const peoples = ['Abir', 'sharif', 'shahed', 'anisa', 'Din-islam', 'Ikra', 'Farhan']
+
+
   return (
     <div className="App">
-      <Person name="Farhan Sharif" email="farhansharif@gmail.com" />
-      <Person name="Anisa Ikra" email="anisa.info@gmail.com" />
+      {/* <Person name="Farhan Sharif" email="farhansharif@gmail.com" />
+      <Person name="Anisa Ikra" email="anisa.info@gmail.com" /> */}
+
+      {/* {
+        peoples.map(people => <li>{people}</li>)
+      } */}
+
+      {
+        peoples.map(people => <Person name={people} />)
+      }
+
     </div>
   );
 }
 
 function Person(props) {
-
   return (
-    <div>
-      <h1>My Name is {props.name} </h1>
-      <p>Email: {props.email}</p>
+    <div className='container'>
+      <h1 >My Name is: {props.name} </h1>
     </div>
   )
 }
+
+
+
+
 
 export default App;
